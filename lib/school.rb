fe385df405ec015c5b388f5df15b545190c1cb62
Kunal <kdshah6593@gmail.com>
2020-11-15 18:22:25 -0500

    commit tree 650a658c844663f8f31c16e31f5ea86c9adceb1a

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
 # code here!
+
+class School
+
+    attr_accessor :roster
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(student, grade)
+        if @roster.keys.include?(grade)
+            @roster[grade] << student
+        else
+            @roster[grade] = []
+            @roster[grade] << student
+        end
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster.sort_by { |grade, students| grade }.to_h
+        @roster.each { |grade, students| students.sort! }
+    end
+
+end
